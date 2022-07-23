afa095ab89930626d9541f49f5d3c4a340ad79cd
devansh2447 <devansh2447@gmail.com>
2022-07-23 11:41:07 +0530

    update gitingnore

--- Interface.java
+++ Interface.java
@@ -1,14 +1,13 @@
-public class Interface
-{
-   //develop interface methods
-   
-   public static void ui(){
+public class Interface {
+   // develop interface methods
+
+   public static void ui() {
       System.out.println("Welcome to the matrix calculator.");
 
    }
 
-   public static Matrix result(String input){
-      //approach: compute brackets first and update the input string.
+   public static Matrix result(String input) {
+      // approach: compute brackets first and update the input string.
       return null;
    }
 }
